342eb22220d6c99c968a50c2011c7f519fe39c1d
flarsi <danil.zap63.dd@gmail.com>
2021-02-26 11:19:31 +0200

    fixed favorite component

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -11,7 +11,7 @@ const Favorite = ({fetchWeather, favorite}) => {
 
     return <Grid container spacing={1} direction="column">
         <Typography variant="h4" component="h2" align="center">Favorite cities</Typography>
-        {Object.keys(favorite).map((key, index) =>
+        {favorite && Object.keys(favorite).map((key, index) =>
             <Grid item key={index}>
                 <Button variant="outlined" fullWidth onClick={HandleSearch(key)}>{key}</Button>
             </Grid>
